dbecf4f5a0d95a21a76acfb27648338e58f774f1
Gaffen <tonedeafmessiah@gmail.com>
2013-06-06 22:40:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniTwit::Application.config.secret_key_base = '3971872e761778c2dc15ca252e12663cc8d41ab2a12abc9f3242d87e2e780022276c3368129364bd2bdc28cd71b9b3d1666aa30c0733137dfbb4654bc7292181'
